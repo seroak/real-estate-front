35dd78d4d34921517309f9a8d512eb0e91c49f6e
lee gyu yeol-mac <i1004gy@gmail.com>
2025-06-06 20:54:36 +0900

    Vercel analytics 설치

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import RQProviders from "./_components/RQProvider";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function RootLayout({
   children,
@@ -11,6 +12,7 @@ export default function RootLayout({
       <body suppressHydrationWarning>
         <RQProviders>
           <main className="max-w-6xl mx-auto px-4 pt-4 pb-10">{children}</main>
+          <Analytics />
         </RQProviders>
       </body>
     </html>
