e1407ac19f09d77314864f872eb38777fc5de713
lee gyu yeol-mac <i1004gy@gmail.com>
2025-06-12 14:23:56 +0900

    useInfinityquery 필요없는 prefetch hydration 제거

--- src/app/(realestate)/page.tsx
+++ src/app/(realestate)/page.tsx
@@ -1,8 +1,13 @@
-import { Suspense } from "react";
 import RealEstateClient from "./_components/RealEstateClient";
-import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query";
-import getRealEstateDatas from "./_lib/getRealEstateDatas";
-import { SearchParams } from "next/dist/server/request/search-params";
+
+type SearchParams = {
+  gu?: string;
+  deposit_min?: string;
+  deposit_max?: string;
+  rent_min?: string;
+  rent_max?: string;
+  dong?: string;
+};
 export default async function RealEstatePage({ searchParams }: { searchParams: Promise<SearchParams> }) {
   const resolvedSearchParams = await searchParams;
   const gu = resolvedSearchParams.gu ? resolvedSearchParams.gu : undefined;
@@ -10,34 +15,18 @@ export default async function RealEstatePage({ searchParams }: { searchParams: P
   const deposit_max = resolvedSearchParams.deposit_max ? resolvedSearchParams.deposit_max : undefined;
   const rent_min = resolvedSearchParams.rent_min ? resolvedSearchParams.rent_min : undefined;
   const rent_max = resolvedSearchParams.rent_max ? resolvedSearchParams.rent_max : undefined;
-  const dongParam = resolvedSearchParams.dong;
-  const dong = dongParam ? (Array.isArray(dongParam) ? dongParam : [dongParam]) : undefined;
-  const queryClient = new QueryClient();
-  await queryClient.prefetchInfiniteQuery({
-    queryKey: ["search", { gu, deposit_min, deposit_max, rent_min, rent_max, dong }],
-    queryFn: ({ pageParam = "1" }) =>
-      getRealEstateDatas({
-        pageParam,
-        queryKey: ["search", { gu, deposit_min, deposit_max, rent_min, rent_max, dong }],
-      }),
-    initialPageParam: "1",
-  });
+  const dong = resolvedSearchParams.dong ? resolvedSearchParams.dong : undefined;
 
-  const dehydratedState = dehydrate(queryClient);
   return (
-    <Suspense fallback={<div>로딩 중...</div>}>
-      <HydrationBoundary state={dehydratedState}>
-        <main className="max-w-6xl mx-auto px-4 pt-4 pb-10">
-          <RealEstateClient
-            gu={gu}
-            dong={dong}
-            deposit_min={deposit_min}
-            deposit_max={deposit_max}
-            rent_min={rent_min}
-            rent_max={rent_max}
-          />
-        </main>
-      </HydrationBoundary>
-    </Suspense>
+    <div>
+      <RealEstateClient
+        gu={gu}
+        dong={dong}
+        deposit_min={deposit_min}
+        deposit_max={deposit_max}
+        rent_min={rent_min}
+        rent_max={rent_max}
+      />
+    </div>
   );
 }
